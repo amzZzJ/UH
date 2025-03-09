6813f2d9d8eba4f9410fd868efb5db1fb74fb114
Amina Dzhalilova <aadzhalilova@edu.hse.ru>
2025-03-09 12:42:29 +0300

    Add .gitignore

---
+++ Config.swift
@@ -0,0 +1,5 @@
+struct Config {
+    static let OAUTH_TOKEN = "y0_AgAAAAA_Z7qeAATuwQAAAAEXbTStAADgsfFcTkhES5l0VNi7NUW4M3pZeg"
+    static let FOLDER_ID = "b1gpla5n2g0jm6f120el"
+    static let API_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+}
